934953d47f84db18fca7896ecfb28ec1b1f686ed
AlexanderBaransky <alexander.baranskiy@yandex.ru>
2020-12-24 17:01:55 +0300

    parts: Reverted back previous XiaomiParts, added strings.xml

--- parts/src/org/lineageos/settings/fod/FodUtils.java
+++ parts/src/org/lineageos/settings/fod/FodUtils.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
- *               2020 The LineageOS Project
+ * Copyright (C) 2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,18 +14,15 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.sensors;
+package org.lineageos.settings.fod;
 
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
 
-public final class SensorsUtils {
-    public static Sensor getSensor(SensorManager sm, String type) {
-        for (Sensor sensor : sm.getSensorList(Sensor.TYPE_ALL)) {
-            if (type.equals(sensor.getStringType())) {
-                return sensor;
-            }
-        }
-        return null;
+public class FodUtils {
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, FodService.class),
+                UserHandle.CURRENT);
     }
 }
