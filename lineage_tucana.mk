fcefdcfe57bb3bd222d4dfb7e3d5d07f0678abe6
AlexanderBaransky <alexander.baranskiy@yandex.ru>
2020-12-26 14:58:22 +0300

    Make tree official

--- lineage_tucana.mk
+++ lineage_tucana.mk
@@ -9,10 +9,7 @@ $(call inherit-product, device/xiaomi/tucana/device.mk)
 # Inherit some
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-# EVO_BUILD_TYPE := UNOFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -25,3 +22,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := Xiaomi/tucana_eea/tucana:10/QKQ1.190825.002/V11.1.1.0.QFDEUXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Project Sakura official things
+SAKURA_MAINTAINER := SanyaPilot
+SAKURA_OFFICIAL := true
